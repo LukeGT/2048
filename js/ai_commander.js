f89a911e3503d24c56f397d997830cc92a6cc11d
Luke Tsekouras <luke.tsekouras@gmail.com>
2014-04-20 00:41:33 +1000

    Dished out quota properly, considered 4 tiles in the final stages

--- js/ai_commander.js
+++ js/ai_commander.js
@@ -47,7 +47,7 @@ AICommander.prototype.think = function () {
     return;
   }
 
-  var best = this.findBest(grid, 40000, 1);
+  var best = this.findBest(grid, 20000, 1);
   console.log(best);
 
   if (best.move === -1) {
@@ -78,7 +78,10 @@ AICommander.prototype.findBest = function (grid, quota, depth) {
     score: 0,
   };
 
-  var two = new Tile({x:0, y:y}, 2);
+  var tile = new Tile({x:0, y:0}, 0);
+
+  var moves = []
+  var move_count = 0;
 
   for (var move = 0; move < 4; ++move) {
 
@@ -86,30 +89,55 @@ AICommander.prototype.findBest = function (grid, quota, depth) {
     clone.move(move);
 
     if (!clone.equals(grid)) {
+      moves[move] = clone
+      ++move_count;
+    } else {
+      moves[move] = null
+    }
+  }
 
+  for (var move = 0; move < 4; ++move) {
+
+    if (moves[move] != null) {
+
+      var moved_grid = moves[move]
       var weight = 0;
       var score = 0;
 
       var empty = 0;
       for (var y = 0; y < 4; ++y) {
         for (var x = 0; x < 4; ++x) {
-          if (clone.cells[x][y] == null) {
+          if (moved_grid.cells[x][y] == null) {
             empty++;
           }
         }
       }
 
-      var new_quota = quota / (4 * empty);
+      var new_quota = quota / (move_count * (empty <= 2 ? empty * 2 : empty));
 
       for (var y = 0; y < 4; ++y) {
         for (var x = 0; x < 4; ++x) {
-          if (clone.cells[x][y] == null) {
-            two.x = x;
-            two.y = y;
-            clone.insertTile(two);
-            score += self.findBest(clone, new_quota, depth + 1).score;
+
+          if (moved_grid.cells[x][y] == null) {
+
+            tile.x = x;
+            tile.y = y;
+            tile.value = 2;
+
+            moved_grid.insertTile(tile);
+            score += self.findBest(moved_grid, new_quota, depth + 1).score;
             weight += 1;
-            clone.removeTile(two);
+            moved_grid.removeTile(tile);
+
+            if (empty <= 2) {
+
+              tile.value = 4;
+
+              moved_grid.insertTile(tile);
+              score += 1/9 * self.findBest(moved_grid, new_quota, depth + 1).score;
+              weight += 1/9;
+              moved_grid.removeTile(tile);
+            }
           }
         }
       }
